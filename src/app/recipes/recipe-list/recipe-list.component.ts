0e0d9399fcc02c54576105a0ac714f51bbc437d4
zwakelem <zwakelemgabhi@gmail.com>
2016-12-15 07:32:32 +0200

    fix bug

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,9 +8,12 @@ import { RecipeItemComponent } from './recipe-item.component';
 })
 export class RecipeListComponent implements OnInit {
   
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+  new Recipe('Schnitzel','very tasty','http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[]),
+  new Recipe('Summer salad','okayish','http://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg',[])
+  ];
+  
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy','Dummy','https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSmVRuVfirCIL6YHo_h6P9x4jdhcvJF-Y11Bijb4539zReDrY61');
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-/*import { ShoppingListAddComponent } from './shopping-list-add.component';*/
+import { Ingredient } from '../recipes/ingredient';
 
 @Component({
   selector: 'rb-shopping-list',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   /*styleUrls: ['./shopping-list.component.css']*/
 })
 export class ShoppingListComponent implements OnInit {
+  
+  items: Ingredient[] = [];
 
   constructor() { }
 
